ec9fe2a18dc17e8aae2826e04c588c6d683beeb4
maurinenami <maurinenami>
2019-06-20 20:00:40 +0300

    included the app inside settings.py

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse,Http404
+
+
+
+
+
+
+
+def welcome(request):
+    return HttpResponse('welcome to instagram')
