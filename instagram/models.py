4e29ef71a8c809fa9004590279c12c4f5447772e
maurinenami <maurinenami>
2019-06-21 10:28:57 +0300

    imported seting in the apps urls.py

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Image(model.Model):
+    image = models.ImageField(upload_to ='insta/' )
+    image_name = models.charField(max_length=30)
+    image_caption = HTMLField()
+    profile = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    likes = models.CharField(max_length =50)
+    comments = models.CharField(max_length =50)
+    def __str__(self):
+        return self.image_name
